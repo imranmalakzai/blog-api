2f54a90f25dd7122b6cd98bb24df0b072962413f
imranmalakzai <afghanimran92@gmail.com>
2026-01-04 19:52:59 +0430

    feat(article): add get archive articles database query

--- src/repository/articals.repsitory.js
+++ src/repository/articals.repsitory.js
@@ -54,3 +54,12 @@ export const changeVisibility = async (articleId, visibility) => {
   );
   return result.affectedRows;
 };
+
+//**Get archive articles */
+export const getArchiveArticles = async (authorId) => {
+  const [rows] = await pool.query(
+    "SELECT * FROM articles WHERE status = ? AND author_id = ? AND deleted_at IS NULL",
+    ["archived", authorId]
+  );
+  return rows;
+};
