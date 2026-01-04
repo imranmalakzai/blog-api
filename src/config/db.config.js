57a68eb9936821da83808822e54d68658fb6fef0
imranmalakzai <afghanimran92@gmail.com>
2026-01-04 06:30:21 +0430

    fetch(db): add database connection -> mysql

---
+++ src/config/db.config.js
@@ -0,0 +1,13 @@
+import mysql from "mysql2/promise";
+
+import { DB_NAME, DB_USER, DB_password, HOST_URI } from "./env.js";
+
+export const pool = mysql.createPool({
+  host: HOST_URI || "localhost",
+  user: DB_USER || "root",
+  password: DB_password || "",
+  database: DB_NAME || "",
+  waitForConnections: true,
+  connectionLimit: 10, // max simultaneous connections
+  queueLimit: 0, // unlimited waiting queries
+});
