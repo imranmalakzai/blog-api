2ac3f37500e9772aa884507b7229c608223b6bd4
imranmalakzai <afghanimran92@gmail.com>
2026-01-05 09:26:35 +0430

    feat(tags): add get all tags database query

--- src/repository/tags.repository.js
+++ src/repository/tags.repository.js
@@ -1,10 +1,16 @@
 import { pool } from "../config/db.config.js";
 
 //** create a new tag */
-export const tags = async (name, slug) => {
+export const createTags = async (name, slug) => {
   const [result] = await pool.query(
     "INSERT INTO tags (name,slug) VALUES (?,?)",
     [name, slug]
   );
   return result.insertId;
+};
+
+//**GET all tages */
+export const getAllTags = async () => {
+  const [rows] = await pool.query("SELECT * FROM tags");
+  return rows;
 };
