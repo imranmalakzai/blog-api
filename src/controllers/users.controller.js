e6af46e8d6c324cc2ab02bf70e55134addc38060
imranmalakzai <afghanimran92@gmail.com>
2026-01-06 09:52:05 +0430

    feat(auth): create login controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import ApiError from "../utils/apiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import { createUserSession } from "../repository/users_session.repository.js";
+import { genearteRefreshToken, generateAccessToken } from "../utils/jwt.js";
 import {
   getUserByEmail,
   userRegistration,
@@ -21,4 +23,50 @@ export const register = asyncHandler(async (req, res) => {
   if (result === 0) throw new ApiError("Internal server error ", 500);
 
   res.status(201).json({ message: "registration successed" });
+});
+
+//** login user as Already registred */
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const userAgent = req.headers["user-agent"];
+  const ipAddress = req.ip;
+
+  //user exist
+  const user = await getUserByEmail(email);
+  if (!user) throw new ApiError("Invalid cridential", 400);
+
+  //compare password
+  const isMatch = await bcrypt.compare(password, user.password_hash);
+  if (!isMatch) throw new ApiError("Invalid cridential", 400);
+
+  //tokens
+  const accessToken = await generateAccessToken(user);
+  const refreshToken = await genearteRefreshToken(user);
+  //user session
+  const userSession = await createUserSession({
+    user_id: user.id,
+    refresh_token: refreshToken,
+    user_agent: userAgent,
+    ip_address: ipAddress,
+  });
+  if (userSession === 0) throw new ApiError("Internal server error");
+  const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  };
+  res
+    .status(200)
+    .cookie("refreshToken", refreshToken, options)
+    .json({
+      accessToken,
+      user: {
+        user_id: user.id,
+        username: user.username,
+        role: user.role,
+        email: user.email,
+        avatar: user.avatar,
+      },
+    });
 });
