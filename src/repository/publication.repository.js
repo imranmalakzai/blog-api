61b396eb68dbbd14dde5644d9d3ba41f8df14a38
imranmalakzai <afghanimran92@gmail.com>
2026-01-06 19:40:43 +0430

    feat(pulication): add create publication database query

---
+++ src/repository/publication.repository.js
@@ -0,0 +1,10 @@
+import { pool } from "../config/db.config.js";
+
+//**create create publications */
+export const create = async (data) => {
+  const [result] = await pool.query(
+    "INSERT INTO publications (name,slug,description,owner_id) VALUES (?,?,?,?)",
+    [data.name, data.slug, data.description, data.owner_id]
+  );
+  return result.insertId;
+};
