1edfbf0f480cebfaa02e552d090399e9b190193e
imranmalakzai <afghanimran92@gmail.com>
2026-01-06 06:30:23 +0430

    feat(env): add JWT Access and Refresh Tokens inside name export function

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -2,4 +2,13 @@ import env from "dotenv";
 
 env.configDotenv({ path: `.env.${"production" || "development"}.local` });
 
-export const { DB_NAME, DB_USER, DB_password, HOST_URI } = process.env;
+export const {
+  DB_NAME,
+  DB_USER,
+  DB_password,
+  HOST_URI,
+  REFRESH_TOKEN,
+  REFRESH_TOKEN_EXPIRE,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_EXPIRE,
+} = process.env;
