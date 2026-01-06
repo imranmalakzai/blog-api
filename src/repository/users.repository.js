08bfe313ff565c73279a096a37077927b316eefb
imranmalakzai <afghanimran92@gmail.com>
2026-01-06 06:45:21 +0430

    fix(registreation): remove sensetive filed from registreation user

--- src/repository/users.repository.js
+++ src/repository/users.repository.js
@@ -3,15 +3,8 @@ import { pool } from "../config/db.config.js";
 //** user registration database query */
 export const userRegistration = async (data) => {
   const result = await pool.query(
-    "INSERT INTO users (username,email,password_hash,bio,avatar_url,role) VALUES (?,?,?,?,?,?)",
-    [
-      data.username,
-      data.email,
-      data.password_hash,
-      data.bio,
-      data.avatar_url,
-      data.role,
-    ]
+    "INSERT INTO users (username,email,password_hash) VALUES (?,?,?)",
+    [data.username, data.email, data.password_hash]
   );
   return result.affectedRows;
 };
