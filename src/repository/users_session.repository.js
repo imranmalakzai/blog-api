08e3ad0a29eed4076f635d5d924aa5527c7685fb
imranmalakzai <afghanimran92@gmail.com>
2026-01-06 09:46:35 +0430

    fix(user_session): remove unwanted filed in user session

--- src/repository/users_session.repository.js
+++ src/repository/users_session.repository.js
@@ -3,7 +3,7 @@ import { pool } from "../config/db.config.js";
 //**Create a user seession */
 export const createUserSession = async (data) => {
   const result = await pool.query(
-    "INSERT INTO user_sessions (user_id,refresh_token,user_agent,ip_address) VALUES (?,?,?,?)",
+    "INSERT INTO user_sessions (user_id,refresh_token,user_agent,ip_address,expires_at) VALUES (?,?,?,?,DATE_ADD(NOW(), INTERVAL 7 DAY))",
     [data.user_id, data.refresh_token, data.user_agent, data.ip_address]
   );
   return result.affectedRows;
