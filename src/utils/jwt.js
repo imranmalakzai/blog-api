05b70dce76925907d54c72c1a67e3beb205b3162
imranmalakzai <afghanimran92@gmail.com>
2026-01-06 06:37:14 +0430

    feat(auth): create generateAccess Token util fuction

---
+++ src/utils/jwt.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import {
+  REFRESH_TOKEN,
+  REFRESH_TOKEN_EXPIRE,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_EXPIRE,
+} from "../config/env.config.js";
+
+//**Genearte access Token */
+export const generateAccessToken = async (user) => {
+  const token = await jwt.sign({ id: user.id, role: user.role }, ACCESS_TOKEN, {
+    expiresIn: REFRESH_TOKEN_EXPIRE,
+  });
+  return token;
+};
