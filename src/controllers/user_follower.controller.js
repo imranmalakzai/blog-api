d10d0b1faa56b6148001e7c5c2b388cd820723e2
imranmalakzai <afghanimran92@gmail.com>
2026-01-08 12:41:19 +0430

    feat(user): create unfollwe user controller

--- src/controllers/user_follower.controller.js
+++ src/controllers/user_follower.controller.js
@@ -1,11 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import ApiError from "../utils/apiError.js";
 import { getUserbyId } from "../repository/users.repository.js";
-import {
-  follow,
-  isFollowing,
-  isFollowing,
-} from "../repository/user_follower.js";
+import { follow, isFollowing, unFollow } from "../repository/user_follower.js";
 
 //** Follow A user */
 export const followUser = asyncHandler(async (req, res) => {
@@ -26,4 +22,21 @@ export const followUser = asyncHandler(async (req, res) => {
   if (!result) throw new ApiError("Internal server error", 500);
 
   res.status(200).json({ message: "following" });
+});
+
+//** Un follow a user */
+export const unfollow = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  const user = await getUserbyId(userId);
+  if (!user) throw new ApiError("user not exist");
+
+  // is following
+  const following = await isFollowing(req.user.id, userId);
+  if (!following) throw new ApiError("please follow first", 403);
+
+  const unfollowed = await unFollow(req.user.id, userId);
+  if (unfollowed === 0) throw new ApiError("Internal server error", 500);
+
+  res.status(200).json({ message: "unfollwed" });
 });
