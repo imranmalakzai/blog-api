f02954ac54f0b39ebd07929df82fa7610ca47a87
imranmalakzai <afghanimran92@gmail.com>
2026-01-09 19:22:59 +0430

    del(artilcle): delelete article rep file from user repo

--- src/repository/users.repository.js
+++ src/repository/users.repository.js
@@ -106,11 +106,3 @@ export const userDeleteAccount = async (userId) => {
   );
   return result.affectedRows;
 };
-
-//**Get all published Articles */
-export const publishedArticles = async () => {
-  const [rows] = await pool.query(
-    "SELECT * FROM articles WHERE status = 'published' AND visibility = 'public' AND deleted_at IS NULL"
-  );
-  return rows;
-};
