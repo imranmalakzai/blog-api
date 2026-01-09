b7e85f23d19d97b87d2677bb1293232b455aff66
imranmalakzai <afghanimran92@gmail.com>
2026-01-09 19:28:56 +0430

    refc(aricle endpoints): update article Api Route endpoints

---
+++ src/routes/article.route.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { auth } from "../middleware/auth.middleware.js";
+import {
+  createNewArticle,
+  publictionArticles,
+  deletePublicationArticle as deleteArticle,
+  publicationArticle,
+  userArticle,
+  articleDelete,
+  getPublishedArticles,
+} from "../controllers/articles.controller.js";
+
+const articleRouter = express.Router({ mergeParams: true });
+articleRouter.use(auth);
+
+//** Publication Articles */
+articleRouter
+  .route("publication/:publicationId/articles")
+  .post(createNewArticle);
+articleRouter
+  .route("publication/:publicationId/articles")
+  .get(publictionArticles);
+articleRouter
+  .route("publication/:publicationId/articles/:articleId")
+  .delete(deleteArticle);
+articleRouter
+  .route("publication/:publicationId/articles/:articleId")
+  .get(publicationArticle);
+articleRouter
+  .route("publication/:publicationSlug-:publicationId/articles")
+  .get(publictionArticles);
+articleRouter
+  .route(
+    "publication/:publicationSlug-:publicationId/articles/:articleSlug-:articleId"
+  )
+  .get(publicationArticle);
+
+//** Public Independent articles */
+articleRouter.route("articles").get(getPublishedArticles);
+articleRouter.route("articles/:articleSlug-:articleId").get(userArticle);
+articleRouter.route("articles/:articleId").get(userArticle);
+articleRouter.route("articles/:articleId").delete(articleDelete);
