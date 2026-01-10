5c824d6557804a8da81cf3e3af415571bddf4270
imranmalakzai <afghanimran92@gmail.com>
2026-01-10 19:01:18 +0430

    feat(article Reaction): add unlike an article database query

--- src/repository/article_rection.repository.js
+++ src/repository/article_rection.repository.js
@@ -8,3 +8,12 @@ export const createLikeArticle = async (data) => {
   );
   return result.insertId;
 };
+
+//**Un like an article */
+export const unlikeArticle = async (articleId, userId) => {
+  const result = await pool.query(
+    "DELETE FROM article_reactions WHERE article_id = ? AND user_id = ?",
+    articleId,
+    userId
+  );
+};
