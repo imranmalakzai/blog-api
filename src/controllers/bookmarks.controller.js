b691e8443e4eba8c29145d30fbcad8091f1fd9a8
imranmalakzai <afghanimran92@gmail.com>
2026-01-11 06:14:30 +0430

    feat(bookmarks):create bookmark on article controller

--- src/controllers/bookmarks.controller.js
+++ src/controllers/bookmarks.controller.js
@@ -1,1 +1,18 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError from "../utils/apiError.js";
+import * as Db from "../repository/bookmarks.repository.js";
 
+//** Book an article */
+export const create = asyncHandler(async (req, res) => {
+  const { articleId } = req.params;
+
+  //check already bookmarked
+  const bookmark = await Db.bookmark(articleId, req.user.id);
+  if (bookmark) throw new ApiError("Already bookmarked", 400);
+
+  //const bookmarked
+  const result = await Db.create(articleId, req.user.id);
+  if (!result) throw new ApiError("Internal server error", 500);
+
+  res.status(200).json({ message: "Article bookmarked successfully" });
+});
