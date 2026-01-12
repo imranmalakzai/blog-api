eff53433bd63ba2a2964cdd22c1bee4c4eebdb5a
imranmalakzai <afghanimran92@gmail.com>
2026-01-12 19:29:54 +0430

    feat(article_tags): create article tags controller

--- src/controllers/article_tags.controller.js
+++ src/controllers/article_tags.controller.js
@@ -1,13 +1,24 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import slugify from "slugify";
 import ApiError from "../utils/apiError.js";
 import * as Db from "../repository/article_tags.repository.js";
 import * as articleDb from "../repository/articals.repsitory.js";
+import * as tagsDb from "../repository/tags.repository.js";
 
 //** add tags in article */
-export const create = asyncHandler(async (req, res, next) => {
+export const create = asyncHandler(async (req, res) => {
   const { articleId } = req.params;
   const { tags } = req.body;
 
-  const article = await articleDb.getArticleById(articleId);
-  if (!article) throw new ApiError("article not exist", 404);
+  if (!Array.isArray(tags)) throw new ApiError("Tage must be an array", 400);
+  //Get existing tags
+  const existingTags = await tagsDb.tags(tags);
+  const tagIds = await existingTags.map((tag) => tag.id);
+
+  //Insert new tags
+  for (const tagI of tagIds) {
+    await Db.create(articleId, tagI);
+  }
+
+  res.status(200).json({ message: "Tags Added successfully" });
 });
