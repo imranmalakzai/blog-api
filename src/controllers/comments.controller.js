1bb49e998e26a347a4931cfd238b618d088c6863
imranmalakzai <afghanimran92@gmail.com>
2026-01-13 06:59:52 +0430

    feat(comments): create update a comment controller

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -18,6 +18,29 @@ export const create = asyncHandler(async (req, res) => {
   res.status(201).json({ message: "comment added successfully" });
 });
 
+//** Update a comment */
+export const update = asyncHandler(async (req, res) => {
+  const { articleId, commentId } = req.params;
+  const { content } = req.body;
+
+  //comment exist
+  const comment = await commentDb.getCommentById(commentId);
+
+  if (!comment || !comment.articleId.toString() !== articleId.toString()) {
+    throw new ApiError("comment or article not exist", 404);
+  }
+
+  // id owner
+  if (comment.user_id.toString() !== req.user.id.toString()) {
+    throw new ApiError("Access denied", 403);
+  }
+
+  const result = await Db.updateComment(content, comment.id);
+  if (!result.lenght) throw new ApiError("Internal server error", 500);
+
+  res.status(200).json({ message: "comment updated successfully" });
+});
+
 //** comment an article comment */
 export const commentCreate = asyncHandler(async (req, res) => {
   const { articleId, commentId } = req.params;
