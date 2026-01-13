9a4cab5a3603db95ab14727ef45174ec77c784c2
imranmalakzai <afghanimran92@gmail.com>
2026-01-13 11:54:00 +0430

    feat(publication): add update publication controller

--- src/controllers/publication.controller.js
+++ src/controllers/publication.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import ApiError from "../utils/apiError.js";
 import * as Db from "../repository/publication.repository.js";
 import slugify from "slugify";
-import { tr } from "zod/locales";
+import { pick } from "zod/mini";
 
 //** create a new publications */
 export const create = asyncHandler(async (req, res) => {
@@ -25,4 +25,30 @@ export const create = asyncHandler(async (req, res) => {
   if (!result.lenght) throw new ApiError("Internal server error, 500");
 
   res.status(201).json({ message: "publication created successfully" });
+});
+
+export const update = asyncHandler(async (req, res) => {
+  const { publicationId } = req.params;
+  const { name, description } = req.body;
+
+  //publication exist
+  const publication = await Db.publicationById(publicationId);
+  if (publication) throw new ApiError("publication not exist", 404);
+
+  if (publication.owner_id.toString() !== req.user.id.toString()) {
+    throw new ApiError("Access denied", 403);
+  }
+
+  const slug = slugify(name, { trim: true, lower: true, strict: true });
+
+  //check for the publication
+  const isExist = await Db.publicationBySlug(slug);
+  if (isExist && isExist.id.toString() !== publication.toString()) {
+    throw new ApiError("publicatin name exist", 400);
+  }
+  const result = await Db.updatePublications({ name, slug, description });
+
+  if (result === 0) throw new ApiError("Internal server error", 500);
+
+  res.status(200).json({ message: "publication updated successfully" });
 });
