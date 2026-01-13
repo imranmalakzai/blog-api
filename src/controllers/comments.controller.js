f1aa584078c291c19dd1e5bcc967118406de5172
imranmalakzai <afghanimran92@gmail.com>
2026-01-13 07:08:27 +0430

    feat(comments): create get all comments of an article controller

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -2,6 +2,7 @@ import * as Db from "../repository/comments.repository.js";
 import ApiError from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler";
 import * as commentDb from "../repository/comments.repository.js";
+import * as articleDb from "../repository/articals.repsitory.js";
 
 //**Add comment to an article */
 export const create = asyncHandler(async (req, res) => {
@@ -62,6 +63,18 @@ export const remove = asyncHandler(async (req, res) => {
   if (!result.lenght) throw new ApiError("Internal server error, 500");
 
   res.status(200).json({ message: "Comment deleted successfully" });
+});
+
+//**Get all comment of on article */
+export const comments = asyncHandler(async (req, res) => {
+  const { articleId } = req.params;
+
+  const article = await articleDb.getArticleById(articleId);
+  if (!article) throw new ApiError("article not exist", 404);
+
+  const comments = await Db.articleComments(articleId);
+
+  res.status(200).json({ comments });
 });
 //** comment an article comment */
 export const commentCreate = asyncHandler(async (req, res) => {
