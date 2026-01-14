dc99f872275a3f55cf91c1fc570af15a5d53da24
imranmalakzai <afghanimran92@gmail.com>
2026-01-14 20:38:09 +0430

    feat(article_viewed): create get an article viewd user in 24 hourse

--- src/repository/article_views.repository.js
+++ src/repository/article_views.repository.js
@@ -2,8 +2,8 @@ import { pool } from "../config/db.config.js";
 //**Create article view  */
 export const create = async (data) => {
   const [rows] = await pool.query(
-    "INSERT INTO article_views (article_id,user_id,ip_address,read_time) VALUES (?,?,?,?)",
-    [data.article_id, data.user_id, data.ip_address, data.read_time]
+    "INSERT INTO article_views (article_id,user_id,ip_address,) VALUES (?,?,?,?)",
+    [data.article_id, data.user_id, data.ip_address]
   );
   return rows;
 };
@@ -15,4 +15,12 @@ export const articleViews = async (articleId) => {
     articleId
   );
   return rows[0].total;
+};
+
+//**Get an article view */
+export const viewedArticle = async (articleId, userId) => {
+  const [rows] = await pool.query(
+    "SELECT id from article_views WHERE article_id = ? and user_id = ? AND created_at = NOW() - INTERVAL 24 HOURS"
+  );
+  return rows[0];
 };
