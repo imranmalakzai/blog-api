ade422238c746b9203b56b7e19663b6f82b136fb
imranmalakzai <afghanimran92@gmail.com>
2026-01-15 07:40:59 +0430

    feat(notifications): add mark read a notification

--- src/repository/notification.repository.js
+++ src/repository/notification.repository.js
@@ -10,10 +10,19 @@ export const create = async(async (data) => {
 });
 
 //** get all notification of a user */
-export const notifications = async(async (userId) => {
+export const notifications = async (userId) => {
   const [rows] = await pool.query(
     "SELECT u.id as userId,u.username,u.avatar_url,n.id,n.type,n.is_read FROM notifications n JOIN users u ON u.id = n.user_id WHERE n.user_id  = ?",
     [userId]
   );
   return rows;
-});
+};
+
+//** Mark notification as read on notification */
+export const readNotification = async (notificationId, userId) => {
+  const result = await pool.query(
+    `UPDATE notifications SET is_read = 'true' WHERE id = ? AND user_id = ?`,
+    [notificationId, userId]
+  );
+  return result.affectedRows;
+};
