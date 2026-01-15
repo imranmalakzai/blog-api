82b416a54b39820ead618d3180ad8e3fc206b110
imranmalakzai <afghanimran92@gmail.com>
2026-01-15 19:10:42 +0430

    feat(loadPublication): create load publication middelware

---
+++ src/middleware/loadPublicatin.middleware.js
@@ -0,0 +1,11 @@
+import { publicationBySlug } from "../repository/publication.repository.js";
+import ApiError from "../utils/apiError.js";
+export const loadPublication = async (req, res, next) => {
+  const { publicationSlug } = req.params;
+  const publication = await publicationSlug(publicationBySlug);
+  if (!publication) {
+    throw new ApiError("publication not exist", 404);
+  }
+  req.publication = publication;
+  next();
+};
