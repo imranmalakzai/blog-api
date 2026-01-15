c4b9b252db73b5a260084d0f403c9b547620e8bf
imranmalakzai <afghanimran92@gmail.com>
2026-01-15 09:41:52 +0430

    fix(notification): fix create notification database query

--- src/repository/notification.repository.js
+++ src/repository/notification.repository.js
@@ -1,13 +1,13 @@
 import { pool } from "../config/db.config.js";
 
 //** create notification */
-export const create = async(async (data) => {
+export const create = async (data) => {
   const [result] = await pool.query(
     "INSERT INTO notifications user_id = ? , actor_id = ?, type = ?, entity_id = ?",
     [data.user_id, data.actor_id, data.type, data.entity_id]
   );
   return result.insertId;
-});
+};
 
 //** get all notification of a user */
 export const notifications = async (userId) => {
