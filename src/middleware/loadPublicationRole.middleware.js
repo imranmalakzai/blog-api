0463deaacc221b235eabbcb865934d1650f21dec
imranmalakzai <afghanimran92@gmail.com>
2026-01-15 19:27:33 +0430

    feat(publication): create publication memeber only middleware

---
+++ src/middleware/loadPublicationRole.middleware.js
@@ -0,0 +1,9 @@
+import { isPublicationMemeber } from "../repository/publication_members.repository.js";
+import ApiError from "../utils/apiError.js";
+
+export const publicationMember = async (req, res, next) => {
+  const memeber = await isPublicationMemeber(req.publication.id, req.user.id);
+  if (!memeber) throw new ApiError("Not publication memeber", 403);
+  req.publicationRole = memeber.role;
+  next();
+};
