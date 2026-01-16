7b86618ba59fdcabcb481f394c1e8ef8f542184e
imranmalakzai <afghanimran92@gmail.com>
2026-01-16 11:17:09 +0430

    feat(article reaction): create article reactions route endpoints

---
+++ src/routes/articleReaction.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { auth } from "../middleware/auth.middleware.js";
+import * as articleReaction from "../controllers/article_reactions.controller.js";
+
+const articleReactionRouter = express.Router({ mergeParams: true });
+
+articleReactionRouter.route("/reactions").post(auth, articleReaction.create);
+articleReaction.route("/reactions").get(auth, articleReaction.likes);
+
+export default articleReactionRouter;
