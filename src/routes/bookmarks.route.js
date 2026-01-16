423d0986c49442361d8b655a129945468724fadd
imranmalakzai <afghanimran92@gmail.com>
2026-01-16 14:23:02 +0430

    feat(bookmarks): create article bookmarks controller

---
+++ src/routes/bookmarks.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { auth } from "../middleware/auth.middleware.js";
+import * as bookmarks from "../controllers/bookmarks.controller.js";
+
+const bookmarksRouter = express.Router();
+
+bookmarksRouter.use(auth);
+bookmarksRouter.route("/bookmars/articles/:id").post(bookmarks.create);
+bookmarksRouter.route("/bookmarks/articles/:id").get(bookmarks.bookmark);
+bookmarksRouter.route("/bookmarks/articles/:id").delete(bookmarks.remove);
+bookmarksRouter.route("/bookmarks").get(bookmarks.bookmarks);
+
+export default bookmarksRouter;
