c9e9dcf444f6c2b316edf6f8fdb3b5501e66b3fe
imranmalakzai <afghanimran92@gmail.com>
2026-01-16 07:08:29 +0430

    refc(article): seperate the article route endpoints and publication article route endpoints for scalibility

--- src/routes/article.route.js
+++ src/routes/article.route.js
@@ -1,9 +1,6 @@
 import express from "express";
 import * as article from "../controllers/article.controller.js";
 import { auth } from "../middleware/auth.middleware.js";
-import { publicationMember } from "../middleware/loadPublicationRole.middleware.js";
-import { loadPublication } from "../middleware/loadPublications.middleware.js";
-import { validMemeber } from "../middleware/validPublicationMemeber.middleware.js";
 
 //**Router endpoints */
 const articleRouter = express.Router();
@@ -16,59 +13,4 @@ articleRouter.route("/articles/:articleSlug").get(article.article);
 articleRouter.route("/articles/:articleSlug").patch(auth, article.update);
 articleRouter.route("/articles/:articleSlug").delete(auth, article.remove);
 
-//** publiction route endpoints */
-
-//create
-articleRouter
-  .route("/publications/:publicationSlug/articles")
-  .post(
-    auth,
-    loadPublication,
-    publicationMember,
-    validMemeber("owner", "editor", "writer"),
-    article.paArticle
-  );
-
-//featch all
-articleRouter
-  .route("/publications/:publicationSlug/articles")
-  .get(
-    auth,
-    loadPublication,
-    publicationMember,
-    validMemeber("owner", "editor", "writer"),
-    article.paArticles
-  );
-
-//featch by slug
-articleRouter
-  .route("/publications/:publicationSlug/articles/:articleSlug")
-  .get(
-    auth,
-    loadPublication,
-    publicationMember,
-    validMemeber("owner", "editor", "writer"),
-    article.paArticle
-  );
-
-//delete by slug
-articleRouter
-  .route("/publications/:publicationSlug/articles/:articleSlug")
-  .delete(
-    auth,
-    loadPublication,
-    publicationMember,
-    validMemeber("owner", "writer"),
-    article.paRemove
-  );
-
-//update by slug
-articleRouter
-  .route("/publications/:publicationSlug/articles/:articleSlug")
-  .patch(
-    auth,
-    loadPublication,
-    publicationMember,
-    validMemeber("owner", "writer"),
-    article.paUpdate
-  );
+export default articleRouter;
