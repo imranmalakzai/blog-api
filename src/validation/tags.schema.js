1eca2da14fe3cbffd70e005b0ccdbc774879b35f
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 09:40:07 +0430

    feat(tags): add update tags validation schema

--- src/validation/tags.schema.js
+++ src/validation/tags.schema.js
@@ -6,3 +6,12 @@ export const create = z.object({
     .min(3, "tag name must be at least 3 characters")
     .max(15, "tag name should be not be more then 15 characters");
 });
+
+//update tags
+export const update = z.object({
+  name: z.string()
+    .min(3, "tag name must be at least 3 characters")
+    .max(15, "tag name should be not be more then 15 characters");
+});
+
+
