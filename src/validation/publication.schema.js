51c3a944443d3140f4eb62309827a8de043d339b
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 09:10:56 +0430

    feat(publicationn): create validation schema for creating publication

---
+++ src/validation/publication.schema.js
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+//create new publication
+export const create = z.object({
+  name: z
+    .string()
+    .min(5, "publication name must be at least 5 characters")
+    .max(100, "publiction name must be less then 100 characters"),
+  description: z
+    .string()
+    .min(25, "publication description must be at least 25 characters")
+    .max(1500, "publiction description must be less then 1500 chracters"),
+});
