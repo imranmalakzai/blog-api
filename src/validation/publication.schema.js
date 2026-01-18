47bfd309f801810810bb7ce2a03e32f8d5a8c948
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 09:24:43 +0430

    feat(publication): create update publication vlidation schema

--- src/validation/publication.schema.js
+++ src/validation/publication.schema.js
@@ -11,3 +11,15 @@ export const create = z.object({
     .min(25, "publication description must be at least 25 characters")
     .max(1500, "publiction description must be less then 1500 chracters"),
 });
+
+//update publication schema
+export const update = z.object({
+  name: z
+    .string()
+    .min(5, "publication name must be at least 5 characters")
+    .max(100, "publiction name must be less then 100 characters"),
+  description: z
+    .string()
+    .min(25, "publication description must be at least 25 characters")
+    .max(1500, "publiction description must be less then 1500 chracters"),
+});
