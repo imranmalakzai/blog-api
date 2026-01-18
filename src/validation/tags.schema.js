327b5c61090dbf4db2effaecf2cd257bf7687a0b
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 09:36:33 +0430

    feat(tags): create validation schema for creating new tags

---
+++ src/validation/tags.schema.js
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+//create tags
+export const create = async () => {
+  name: z.string()
+    .min(3, "tag name must be at least 3 characters")
+    .max(15, "tag name should be not be more then 15 characters");
+};
