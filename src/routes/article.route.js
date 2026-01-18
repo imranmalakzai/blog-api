7da6e0650f176c9eaa76609689dcd78b321bd403
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 10:59:47 +0430

    feat(articles): add create article validation schema file inside the create Article API route endpoint

--- src/routes/article.route.js
+++ src/routes/article.route.js
@@ -1,13 +1,19 @@
 import express from "express";
 import * as article from "../controllers/article.controller.js";
 import { auth } from "../middleware/auth.middleware.js";
+import * as schema from "../validation/articles.schema.js";
+import { validate } from "../config/zod.config.js";
 
 //**Router endpoints */
 const articleRouter = express.Router();
 
 //** global route endpoints */
 
-articleRouter.route("/articles").post(auth, article.create);
+//create article
+articleRouter
+  .route("/articles")
+  .post(auth, validate(schema.create), article.create);
+
 articleRouter.route("/articles").get(article.articles);
 articleRouter.route("/articles/:articleSlug").get(article.article);
 articleRouter.route("/articles/:articleSlug").patch(auth, article.update);
