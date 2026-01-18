e4a6dd559a41a5c88e351da6a98438625d39b8be
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 10:35:29 +0430

    feat(comments): create validation schema file for validate the create a new comment

---
+++ src/validation/comment.schema.js
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+//create comment
+export const create = z.object({
+  article_id: z.number().int().positive(),
+  user_id: z.number().int().positive(),
+  parent_id: z.number().int().positive().optional,
+  content: z
+    .string()
+    .min(1)
+    .max(100, "comment lenght must be less then 100 characters"),
+});
