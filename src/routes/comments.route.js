4a8acc18bada4449a997007a878f6a911d4f73ec
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 10:37:00 +0430

    feat(comments): add create comment validation schema file inside the create comment route endpoint

--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -1,13 +1,15 @@
 import express from "express";
 import * as comment from "../controllers/comments.controller.js";
 import { auth } from "../middleware/auth.middleware.js";
+import * as schema from "../validation/comment.schema.js";
+import { validate } from "../config/zod.config.js";
 
 const commentRouter = express.Router();
 
 //create a comment
 commentRouter
   .route("/articles/:articleSlug/comments")
-  .post(auth, comment.create);
+  .post(auth, validate(schema.create), comment.create);
 
 // Get all comments
 commentRouter.route("/articles/:articleSlug/comments").get(comment.comments);
