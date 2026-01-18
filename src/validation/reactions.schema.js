4188a9b2832f032826d4de5173ac0a52d361f508
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 10:27:42 +0430

    feat(reaction): create update vavlidation schema

--- src/validation/reactions.schema.js
+++ src/validation/reactions.schema.js
@@ -7,3 +7,11 @@ export const create = z.object({
     .min(3, "rection name must be at least 3 characters")
     .max(10, "reaction should not be more then 10 characters"),
 });
+
+//create reaciton
+export const update = z.object({
+  name: z
+    .string()
+    .min(3, "rection name must be at least 3 characters")
+    .max(10, "reaction should not be more then 10 characters"),
+});
