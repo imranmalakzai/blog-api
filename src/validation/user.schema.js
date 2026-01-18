29bc631a6e95498e86c118b4f2a6fbe6d8c00986
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 17:05:12 +0430

    refc(users): update user validation schema

--- src/validation/user.schema.js
+++ src/validation/user.schema.js
@@ -3,7 +3,7 @@ import z from "zod";
 //registration schema
 export const register = z.object({
   username: z
-    .string()
+    .string("username is required")
     .min(3, "Username must be at least 3 characters")
     .max(30, "Username must be less than 30 characters")
     .regex(
@@ -11,26 +11,14 @@ export const register = z.object({
       "Username can only contain letters, numbers, and _"
     ),
 
-  email: z.string().email("Invalid email address"),
+  email: z.string("Email address is required").email("Invalid email address"),
 
   password: z
-    .string()
+    .string("password is required")
     .min(8, "Password must be at least 8 characters")
     .max(100)
     .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
     .regex(/[0-9]/, "Password must contain at least one number"),
-
-  bio: z
-    .string()
-    .max(160, "Bio must be under 160 characters")
-    .optional()
-    .or(z.literal("")),
-
-  avatar_url: z
-    .string()
-    .url("Avatar must be a valid URL")
-    .optional()
-    .or(z.literal("")),
 });
 
 //Login schema
