47bb05a63bb9f3511658b2cc0ae758e246ce569e
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 11:01:43 +0430

    feat(article schema): create, update article validation schema

--- src/validation/articles.schema.js
+++ src/validation/articles.schema.js
@@ -25,3 +25,19 @@ export const create = z.object({
       message: "Invalid visiblity please select public || privit || unlisted",
     }),
 });
+
+//update article
+export const update = z.object({
+  title: z
+    .string()
+    .min(5, "title must be at least 5 characters")
+    .max(50, "title should not be more then 50 characters"),
+  content: z
+    .string()
+    .min(10)
+    .max(2500, "article must not be more then 2500 characters"),
+  excerpt: z
+    .string()
+    .min(10)
+    .max(250, "please excerpt should be not more then 250 characters"),
+});
