9be4d0a0011f900b3124ed5b93573cb3f84e1f99
imranmalakzai <afghanimran92@gmail.com>
2026-01-18 10:25:20 +0430

    feat(reactions): add create new ruction validation shcmea

---
+++ src/validation/reactions.schema.js
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+//create reaciton
+export const create = z.object({
+  name: z
+    .string()
+    .min(3, "rection name must be at least 3 characters")
+    .max(10, "reaction should not be more then 10 characters"),
+});
