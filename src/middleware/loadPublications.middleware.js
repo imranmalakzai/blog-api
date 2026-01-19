e2c32a83778d4a536a6cc82e1020592037238370
imranmalakzai <afghanimran92@gmail.com>
2026-01-19 10:30:20 +0430

    refc(middleware): fix load publication middleware

--- src/middleware/loadPublications.middleware.js
+++ src/middleware/loadPublications.middleware.js
@@ -2,7 +2,7 @@ import { publicationBySlug } from "../repository/publication.repository.js";
 import ApiError from "../utils/apiError.js";
 export const loadPublication = async (req, res, next) => {
   const { publicationSlug } = req.params;
-  const publication = await publicationSlug(publicationBySlug);
+  const publication = await publicationBySlug(publicationSlug);
   if (!publication) {
     throw new ApiError("publication not exist", 404);
   }
