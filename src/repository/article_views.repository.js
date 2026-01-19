2c9a7fc0ad6e84b28d74a4939f72afbd7826cf54
imranmalakzai <afghanimran92@gmail.com>
2026-01-19 19:30:33 +0430

    refactor(article view): debug and fix creat article view database query

--- src/repository/article_views.repository.js
+++ src/repository/article_views.repository.js
@@ -1,18 +1,18 @@
 import { pool } from "../config/db.config.js";
 //**Create article view  */
 export const create = async (data) => {
-  const [rows] = await pool.query(
-    "INSERT INTO article_views (article_id,user_id,ip_address,) VALUES (?,?,?,?)",
-    [data.article_id, data.user_id, data.ip_address]
+  const [result] = await pool.query(
+    "INSERT INTO article_views (article_id,user_id,ip_address) VALUES (?,?,?)",
+    [data.article_id, data.user_id, data.ip_address],
   );
-  return rows;
+  return result.insertId;
 };
 
 //**Get an article total viwes */
 export const articleViews = async (articleId) => {
   const [rows] = await pool.query(
     "SELECT count(*) As total FROM article_views WHERE article_id = ?",
-    articleId
+    articleId,
   );
   return rows[0].total;
 };
@@ -21,7 +21,7 @@ export const articleViews = async (articleId) => {
 export const viewedArticle = async (articleId, userId) => {
   const [rows] = await pool.query(
     "SELECT id from article_views WHERE article_id = ? and user_id = ? AND created_at > NOW() - INTERVAL 24 HOUR LIMIT 1",
-    [articleId, userId]
+    [articleId, userId],
   );
   return rows[0];
 };
