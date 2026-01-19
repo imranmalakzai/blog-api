eef5be33a595435f996f57c5f1b2123481fba11e
imranmalakzai <afghanimran92@gmail.com>
2026-01-19 08:02:23 +0430

    fix(notifiction): fix create notification database query

--- src/repository/notification.repository.js
+++ src/repository/notification.repository.js
@@ -3,17 +3,17 @@ import { pool } from "../config/db.config.js";
 //** create notification */
 export const create = async (data) => {
   const [result] = await pool.query(
-    "INSERT INTO notifications user_id = ? , actor_id = ?, type = ?, entity_id = ?",
-    [data.user_id, data.actor_id, data.type, data.entity_id]
+    "INSERT INTO notifications (user_id,actor_id,type,entity_id) VALUES (?,?,?,?)",
+    [data.user_id, data.actor_id, data.type, data.entity_id],
   );
-  return result.insertId;
+  return result.affectedRows;
 };
 
 //** get all notification of a user */
 export const notifications = async (userId) => {
   const [rows] = await pool.query(
     "SELECT u.id as userId,u.username,u.avatar_url,n.id,n.type,n.is_read FROM notifications n JOIN users u ON u.id = n.user_id WHERE n.user_id  = ?",
-    [userId]
+    [userId],
   );
   return rows;
 };
@@ -22,7 +22,7 @@ export const notifications = async (userId) => {
 export const readNotification = async (notificationId, userId) => {
   const result = await pool.query(
     `UPDATE notifications SET is_read = true WHERE id = ? AND user_id = ?`,
-    [notificationId, userId]
+    [notificationId, userId],
   );
   return result.affectedRows;
 };
@@ -31,7 +31,7 @@ export const readNotification = async (notificationId, userId) => {
 export const readNotifications = async (userId) => {
   const result = await pool.query(
     `UPDATE notifications SET is_read = true AND user_id = ?`,
-    [userId]
+    [userId],
   );
   return result.affectedRows;
 };
@@ -40,7 +40,7 @@ export const readNotifications = async (userId) => {
 export const remove = async (notificationId, userId) => {
   const result = await pool.query(
     "DELETE FROM notifications WHERE notification_id = ? AND user_id = ?",
-    [notificationId, userId]
+    [notificationId, userId],
   );
   return result.affectedRows;
 };
@@ -49,7 +49,7 @@ export const remove = async (notificationId, userId) => {
 export const removeAll = async (userId) => {
   const result = await pool.query(
     "DELETE * FRON notifications WHERE user_id = ?",
-    [userId]
+    [userId],
   );
   return result.affectedRows;
 };
