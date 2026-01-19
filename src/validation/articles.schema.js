ec5012978e535b3537fd15fc02554f71e7c91bbd
imranmalakzai <afghanimran92@gmail.com>
2026-01-19 17:59:22 +0430

    fix(article): fix create article validation schema

--- src/validation/articles.schema.js
+++ src/validation/articles.schema.js
@@ -3,21 +3,21 @@ import { z } from "zod";
 //create article
 export const create = z.object({
   title: z
-    .string()
+    .string("title is required")
     .min(5, "title must be at least 5 characters")
     .max(50, "title should not be more then 50 characters"),
   content: z
-    .string()
+    .string("content is required")
     .min(10)
     .max(2500, "article must not be more then 2500 characters"),
   excerpt: z
-    .string()
+    .string("excerpt is required")
     .min(10)
     .max(250, "please excerpt should be not more then 250 characters"),
   status: z
-    .enum(["draft", "publish", "archive"])
-    .refine((val) => ["publish", "draft", "archive"].includes(val), {
-      message: "Invalid status please select publish || draft || archive",
+    .enum(["draft", "published"])
+    .refine((val) => ["published", "draft"].includes(val), {
+      message: "Invalid status please select published || draft ",
     }),
   visibility: z
     .enum(["public", "privite", "unlisted"])
