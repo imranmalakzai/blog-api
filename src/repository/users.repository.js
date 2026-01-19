943e6d0739a3340eea0157ec3f52694d8f436034
imranmalakzai <afghanimran92@gmail.com>
2026-01-19 07:20:22 +0430

    fix(users): fix get user by username database query

--- src/repository/users.repository.js
+++ src/repository/users.repository.js
@@ -92,7 +92,7 @@ export const updateRole = async (role, userId) => {
 //**Get user by username */
 export const getUserByUsername = async (username) => {
   const [rows] = await pool.query(
-    "SELECT id,username,email,role FROM users WHERE deleted_at IS NULL",
+    "SELECT id,username,email,role FROM users WHERE deleted_at IS NULL AND username = ?",
     [username],
   );
   return rows[0];
