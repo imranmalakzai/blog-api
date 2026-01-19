5f6655011a6ab951aa267ddb3f172050dd8060bb
imranmalakzai <afghanimran92@gmail.com>
2026-01-19 16:05:59 +0430

    feat(articles) add article API root route endpoint into the app.js

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import { swaggerSpec } from "./config/swagger.config.js";
 //** import API endpoints  */
 import userRouter from "./routes/users.route.js";
 import tagsRouter from "./routes/tags.route.js";
+import articleRouter from "./routes/article.route.js";
 import reactionRouter from "./routes/reactions.route.js";
 import publicationRouter from "./routes/publication.route.js";
 
@@ -27,6 +28,7 @@ app.use("/api/", userRouter);
 app.use("/api/", publicationRouter);
 app.use("/api/", tagsRouter);
 app.use("/api/", reactionRouter);
+app.use("/api/", articleRouter);
 
 // swagger docs
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
