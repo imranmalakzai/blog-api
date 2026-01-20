08ee4abe9a537d4d344600a340c8a7cd85980f9a
imranmalakzai <afghanimran92@gmail.com>
2026-01-20 09:43:23 +0430

    pref(middleware): add try catch for error handling inside publication memeber middleware

--- src/middleware/loadPublicationRole.middleware.js
+++ src/middleware/loadPublicationRole.middleware.js
@@ -2,8 +2,12 @@ import { isPublicationMemeber } from "../repository/publication_members.reposito
 import ApiError from "../utils/apiError.js";
 
 export const publicationMember = async (req, res, next) => {
-  const memeber = await isPublicationMemeber(req.publication.id, req.user.id);
-  if (!memeber) throw new ApiError("Not publication memeber", 403);
-  req.publicationRole = memeber.role;
-  next();
+  try {
+    const memeber = await isPublicationMemeber(req.publication.id, req.user.id);
+    if (!memeber) throw new ApiError("Not publication memeber", 403);
+    req.publicationRole = memeber.role;
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
