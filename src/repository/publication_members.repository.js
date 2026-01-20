31a386a113ec47d5b131d7cd0299a687ae72a639
imranmalakzai <afghanimran92@gmail.com>
2026-01-20 09:57:56 +0430

    refc(db): optimize create publication memeber database query

--- src/repository/publication_members.repository.js
+++ src/repository/publication_members.repository.js
@@ -3,8 +3,8 @@ import { pool } from "../config/db.config.js";
 //** Add a memeber to the publication  */
 export const createPublicationMember = async (data) => {
   const [result] = await pool.query(
-    "INSERT INTO publication_members (publication_id,user_id)  VALUES (?,?)",
-    [data.publication_id, data.user_id]
+    "INSERT INTO publication_members (publication_id,user_id,role)  VALUES (?,?,?)",
+    [data.publication_id, data.user_id, data.role],
   );
   result.insertId;
 };
@@ -13,7 +13,7 @@ export const createPublicationMember = async (data) => {
 export const deletePublicationMember = async (user_id) => {
   const result = await pool.query(
     "DELETE FROM publication_members WHERE user_id = ?",
-    [user_id]
+    [user_id],
   );
   return result.affectedRows;
 };
@@ -22,7 +22,7 @@ export const deletePublicationMember = async (user_id) => {
 export const changePublicationMemberRole = async (role, user_id) => {
   const result = await pool.query(
     "UPDATE publication_memebers SET role =  ? WHERE user_id = ?",
-    [role, user_id]
+    [role, user_id],
   );
   return result.affectedRows;
 };
@@ -31,7 +31,7 @@ export const changePublicationMemberRole = async (role, user_id) => {
 export const publicationMemebers = async (publicationId) => {
   const [rows] = await pool.query(
     "SELECT u.id as userId,u.username,u.avatar_url FROM publication_members pm JOIN  users u ON u.id = pm.user_id  WHERE publication_id = ? AND role IS NOT null",
-    publicationId
+    publicationId,
   );
   return rows;
 };
@@ -41,7 +41,7 @@ export const userPulications = async (userId) => {
   const [rows] = pool.query(
     `SELECT * FROM publication_members JOIN publications ON publication_members.publication_id = publications.id WHERE publication_members.user_id = ? `[
       userId
-    ]
+    ],
   );
   return rows;
 };
@@ -50,7 +50,7 @@ export const userPulications = async (userId) => {
 export const isPublicationMemeber = async (publicationId, userId) => {
   const [rows] = await pool.query(
     `SELECT * FROM publication_memebers WHERE publication_id = ? AND user_id = ?`,
-    [publicationId, userId]
+    [publicationId, userId],
   );
   return rows[0];
 };
@@ -59,7 +59,7 @@ export const isPublicationMemeber = async (publicationId, userId) => {
 export const publicationFollowers = async (publicationId) => {
   const [rows] = await pool.query(
     "SELECT * FROM publication_members WHERE role IS NULL AND publication_id = ?",
-    [publicationId]
+    [publicationId],
   );
   return rows;
 };
