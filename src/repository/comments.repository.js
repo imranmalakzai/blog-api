c234c3a5f5f4ffe47cf0c23ecf1e4db57a1c7879
imranmalakzai <afghanimran92@gmail.com>
2026-01-21 17:30:04 +0430

    fix(db): correct create comment database query

--- src/repository/comments.repository.js
+++ src/repository/comments.repository.js
@@ -3,8 +3,8 @@ import { pool } from "../config/db.config.js";
 //** create a comment */
 export const createComment = async (data) => {
   const [result] = await pool.query(
-    `INSERT INTO comments (article_id,user_id,parent_id,content) VALUES (?,?,?,?,?)`,
-    [data.article_id, data.user_id, data.parent_id || null, data.content],
+    `INSERT INTO comments (article_id,user_id,parent_id,content) VALUES (?,?,?,?)`,
+    [data.article_id, data.user_id, data.parent_id, data.content],
   );
   return result.insertId;
 };
