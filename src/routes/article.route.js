b5ddec84ead70f1f7b0928498810ef68e783665f
imranmalakzai <afghanimran92@gmail.com>
2026-01-21 20:58:42 +0430

    fix(routes): add middleware inside article route endpoint

--- src/routes/article.route.js
+++ src/routes/article.route.js
@@ -3,6 +3,7 @@ import * as article from "../controllers/article.controller.js";
 import { auth } from "../middleware/auth.middleware.js";
 import * as schema from "../validation/articles.schema.js";
 import { validate } from "../config/zod.config.js";
+import { articleMiddleware } from "../middleware/article.middleware.js";
 
 //**Router endpoints */
 const articleRouter = express.Router();
@@ -15,13 +16,15 @@ articleRouter.route("/").post(auth, validate(schema.create), article.create);
 articleRouter.route("/").get(article.articles);
 
 //get article by slug
-articleRouter.route("/:articleSlug").get(article.article);
+articleRouter.route("/:articleSlug").get(articleMiddleware, article.article);
 
 //update article
 articleRouter
   .route("/:articleSlug")
-  .patch(auth, validate(schema.update), article.update);
+  .patch(auth, articleMiddleware, validate(schema.update), article.update);
 
-articleRouter.route("/:articleSlug").delete(auth, article.remove);
+articleRouter
+  .route("/:articleSlug")
+  .delete(auth, articleMiddleware, article.remove);
 
 export default articleRouter;
