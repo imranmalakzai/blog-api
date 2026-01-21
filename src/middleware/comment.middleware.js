55141ab66413ddebd2cbd186de6b8603269f0260
imranmalakzai <afghanimran92@gmail.com>
2026-01-21 17:05:05 +0430

    refc(comment): attach check comment belongs to article

--- src/middleware/comment.middleware.js
+++ src/middleware/comment.middleware.js
@@ -1,10 +1,24 @@
+/***
+ *
+ * Every comment belong to an article so here we check for tow things
+ * 1: comment exist ?
+ * 2: Dose commen belong to the article
+ *
+ * req.article contain the article // You can use seperate query here too the comment and article
+ */
+
 import ApiError from "../utils/apiError.js";
 import * as Comment from "../repository/comments.repository.js";
 export const commentMiddleware = async (req, res, next) => {
   const { commentId } = req.params;
   try {
     const comment = await Comment.getCommentById(commentId);
-    if (!comment) throw new ApiError("Comment not exist", 404);
+    if (
+      !comment ||
+      !comment.articleId.toString() !== req.article.id.toString()
+    ) {
+      throw new ApiError("comment or article not exist", 404);
+    }
     req.comment = comment;
     next();
   } catch (error) {
