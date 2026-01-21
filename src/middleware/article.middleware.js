55a5288c42bf6d8f975d0e2b6d733500db7daf2d
imranmalakzai <afghanimran92@gmail.com>
2026-01-21 13:31:55 +0430

    feat(middleware): add article exist middlewares

---
+++ src/middleware/article.middleware.js
@@ -0,0 +1,16 @@
+import ApiError from "../utils/apiError.js";
+import * as Article from "../repository/articals.repsitory.js";
+
+export const articleMiddleware = async (req, res, next) => {
+  try {
+    const { articleSlug } = req.params;
+
+    const article = await Article.getArticleBySlug(articleSlug);
+    if (!article) throw new ApiError("Article not exist");
+
+    res.article = article;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
