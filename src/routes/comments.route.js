94941f946e182d00a3cc19388675b387d4752cb5
imranmalakzai <afghanimran92@gmail.com>
2026-01-21 16:57:44 +0430

    feat(comment): attach comment exist inside the comments API route endpoints

--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as comment from "../controllers/comments.controller.js";
 import * as schema from "../validation/comment.schema.js";
+import { commentMiddleware } from "../middleware/comment.middleware.js";
 import { validate } from "../config/zod.config.js";
 
 const commentRouter = express.Router({ mergeParams: true });
@@ -12,14 +13,14 @@ commentRouter.route("/").post(validate(schema.create), comment.create);
 commentRouter.route("/").get(comment.comments);
 
 //get a comment
-commentRouter.route("/:commentId").get(comment.comment);
+commentRouter.route("/:commentId").get(commentMiddleware, comment.comment);
 
 //delete a comment
-commentRouter.route("/:commentId").delete(comment.remove);
+commentRouter.route("/:commentId").delete(commentMiddleware, comment.remove);
 
 //update a comment
 commentRouter
   .route("/:commentId")
-  .patch(validate(schema.update), comment.update);
+  .patch(commentMiddleware, validate(schema.update), comment.update);
 
 export default commentRouter;
