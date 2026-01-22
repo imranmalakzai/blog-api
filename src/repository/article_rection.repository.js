a247726e4deb19e55fb36eb4d1f874b402789786
imranmalakzai <afghanimran92@gmail.com>
2026-01-22 06:16:30 +0430

    pref(db): optimize  react to an article database query

--- src/repository/article_rection.repository.js
+++ src/repository/article_rection.repository.js
@@ -4,7 +4,7 @@ import { pool } from "../config/db.config.js";
 export const createLikeArticle = async (data) => {
   const [result] = await pool.query(
     "INSERT INTO article_reactions (articleId,user_id,reaction_id) VALUES (?,?,?)",
-    [data.articleId, data.user_id, data.reaction_id]
+    [data.articleId, data.user_id, data.reaction_id],
   );
   return result.insertId;
 };
@@ -14,7 +14,7 @@ export const unlikeArticle = async (articleId, userId) => {
   const result = await pool.query(
     "DELETE FROM article_reactions WHERE article_id = ? AND user_id = ?",
     articleId,
-    userId
+    userId,
   );
   return result.affectedRows;
 };
@@ -23,16 +23,16 @@ export const unlikeArticle = async (articleId, userId) => {
 export const usersLikedArticle = async (articleId) => {
   const [rows] = await pool.query(
     "SELECT * FROM article_reactions WHERE article_id = ?",
-    [articleId]
+    [articleId],
   );
   return rows;
 };
 
 //**user reaction */
-export const userReacion = async (userId, articleId) => {
+export const userReaction = async (userId, articleId) => {
   const [rows] = await pool.query(
     "SELECT id from article_reactions where article_id ? and user_id ?",
-    [articleId, userId]
+    [articleId, userId],
   );
   return rows[0];
 };
@@ -41,7 +41,7 @@ export const userReacion = async (userId, articleId) => {
 export const remove = async (userId, articleId) => {
   const result = await pool.query(
     "DELETE FROM article_reactions WHERE article_id = ? and user_id = ?",
-    [articleId, userId]
+    [articleId, userId],
   );
   return result.affectedRows;
 };
@@ -50,7 +50,7 @@ export const remove = async (userId, articleId) => {
 export const update = async (userId, articleId, reactionId) => {
   const result = await pool.query(
     "UPDATE article_reactions SET reaction_id = ? WHERE user_id = ? and article_id = ?",
-    [reactionId, userId, articleId]
+    [reactionId, userId, articleId],
   );
   return result.affectedRows;
 };
