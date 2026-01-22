b10604dd406820e35dd8e345cc0d8fd33bcb5c5c
imranmalakzai <afghanimran92@gmail.com>
2026-01-22 06:19:52 +0430

    fix: remove full paths from article reactio middleware

--- src/routes/articleReaction.route.js
+++ src/routes/articleReaction.route.js
@@ -4,7 +4,7 @@ import * as articleReaction from "../controllers/article_reactions.controller.js
 
 const articleReactionRouter = express.Router({ mergeParams: true });
 
-articleReactionRouter.route("/reactions").post(auth, articleReaction.create);
-articleReaction.route("/reactions").get(auth, articleReaction.likes);
+articleReactionRouter.route("/").post(auth, articleReaction.create);
+articleReaction.route("/").get(auth, articleReaction.likes);
 
 export default articleReactionRouter;
