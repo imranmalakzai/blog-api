79d349c7805c3664801c32fc3607cff197a17fae
imranmalakzai <afghanimran92@gmail.com>
2026-01-22 09:35:17 +0430

    feat(validation): add validation schema inside react to article controller

--- src/routes/articleReaction.route.js
+++ src/routes/articleReaction.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { auth } from "../middleware/auth.middleware.js";
 import * as articleReaction from "../controllers/article_reactions.controller.js";
+import { validate } from "../config/zod.config.js";
+import * as schema from "../validation/articleReaction.schema.js";
 
 const articleReactionRouter = express.Router({ mergeParams: true });
 
-articleReactionRouter.route("/").post(auth, articleReaction.react);
-articleReactionRouter.route("/").get(auth, articleReaction.likes);
+articleReactionRouter
+  .route("/")
+  .post(auth, validate(schema.react), articleReaction.react);
+articleReactionRouter.route("/").get(articleReaction.likes);
 
 export default articleReactionRouter;
