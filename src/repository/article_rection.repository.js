9df944d3ef79ce55cc4b92bbb69f989252cc60d3
imranmalakzai <afghanimran92@gmail.com>
2026-01-22 09:52:04 +0430

    pref(db): optimize article reactions database queries

--- src/repository/article_rection.repository.js
+++ src/repository/article_rection.repository.js
@@ -11,9 +11,37 @@ export const createLikeArticle = async (data) => {
 
 //**Get all users React to an articles */
 export const usersLikedArticle = async (articleId) => {
+  console.log(articleId);
   const [rows] = await pool.query(
-    "SELECT * FROM article_reactions WHERE article_id = ?",
+    "SELECT r.name as reaction,u.id as userId,u.username,u.avatar_url from article_reactions a JOIN users u ON a.user_id = u.id JOIN reactions r ON a.reaction_id = r.id  where a.article_id = ?",
     [articleId],
   );
   return rows;
+};
+
+//**user reaction */
+export const userReaction = async (userId, articleId) => {
+  const [rows] = await pool.query(
+    "SELECT * from article_reactions where article_id = ? and user_id = ?",
+    [articleId, userId],
+  );
+  return rows[0];
+};
+
+//**Remove Reaction from an article */
+export const remove = async (userId, articleId) => {
+  const result = await pool.query(
+    "DELETE FROM article_reactions WHERE article_id = ? and user_id = ?",
+    [articleId, userId],
+  );
+  return result.affectedRows;
+};
+
+//**Update reaction an article */
+export const update = async (userId, articleId, reactionId) => {
+  const result = await pool.query(
+    "UPDATE article_reactions SET reaction_id = ? WHERE user_id = ? and article_id = ?",
+    [reactionId, userId, articleId],
+  );
+  return result.affectedRows;
 };
