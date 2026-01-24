9802565ee4fb1ca3623de732013a0e9b0145e84c
imranmalakzai <afghanimran92@gmail.com>
2026-01-24 08:19:25 +0430

    feat(article): attach check article exis middleware

--- src/routes/bookmarks.route.js
+++ src/routes/bookmarks.route.js
@@ -1,13 +1,21 @@
 import express from "express";
 import { auth } from "../middleware/auth.middleware.js";
 import * as bookmarks from "../controllers/bookmarks.controller.js";
+import { articleMiddleware } from "../middleware/article.middleware.js";
 
 const bookmarksRouter = express.Router();
 
 bookmarksRouter.use(auth);
-bookmarksRouter.route("/bookmarks/articles/:id").post(bookmarks.create);
-bookmarksRouter.route("/bookmarks/articles/:id").get(bookmarks.bookmark);
-bookmarksRouter.route("/bookmarks/articles/:id").delete(bookmarks.remove);
+
+bookmarksRouter
+  .route("/bookmarks/articles/:articleSlug")
+  .post(articleMiddleware, bookmarks.create);
+bookmarksRouter
+  .route("/bookmarks/articles/:articleSlug")
+  .get(articleMiddleware, bookmarks.bookmark);
+bookmarksRouter
+  .route("/bookmarks/articles/:articleSlug")
+  .delete(articleMiddleware, bookmarks.remove);
 bookmarksRouter.route("/bookmarks").get(bookmarks.bookmarks);
 
 export default bookmarksRouter;
