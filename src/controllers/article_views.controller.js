0a10547b225a793275fdbbf0e1517b5ad6963662
imranmalakzai <afghanimran92@gmail.com>
2026-01-24 16:22:29 +0430

    fix : article views controller

--- src/controllers/article_views.controller.js
+++ src/controllers/article_views.controller.js
@@ -1,16 +1,8 @@
 import * as View from "../repository/article_views.repository.js";
-import * as articleDb from "../repository/articals.repsitory.js";
-import ApiError from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 //**Count article views */
 export const articleViews = asyncHandler(async (req, res) => {
-  const { articleSlug } = req.params;
-
-  //article exist
-  const article = await articleDb.getArticleBySlug(articleSlug);
-  if (!article) throw new ApiError("Article not exist", 404);
-
-  const views = await View.viewedArticle(article.id);
+  const views = await View.articleViews(req.article.id);
   res.status(200).json({ views });
 });
