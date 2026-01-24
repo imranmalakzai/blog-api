5b9446f7368e83e00319b23fefb2e69f885f40d5
imranmalakzai <afghanimran92@gmail.com>
2026-01-24 07:44:34 +0430

    fix(bookmarks): exchange article by id to article by slug feature

--- src/controllers/bookmarks.controller.js
+++ src/controllers/bookmarks.controller.js
@@ -4,14 +4,12 @@ import * as Db from "../repository/bookmarks.repository.js";
 
 //** Book an article */
 export const create = asyncHandler(async (req, res) => {
-  const { articleId } = req.params;
-
   //check already bookmarked
-  const bookmark = await Db.bookmark(articleId, req.user.id);
+  const bookmark = await Db.bookmark(req.article.id, req.user.id);
   if (bookmark) throw new ApiError("Already bookmarked", 400);
 
   //const bookmarked
-  const result = await Db.create(articleId, req.user.id);
+  const result = await Db.create(req.article.id, req.user.id);
   if (!result) throw new ApiError("Internal server error", 500);
 
   res.status(200).json({ message: "Article bookmarked successfully" });
@@ -19,14 +17,12 @@ export const create = asyncHandler(async (req, res) => {
 
 //**UnBookmark article */
 export const remove = asyncHandler(async (req, res) => {
-  const { articleId } = req.params;
-
   //check bookmarked exist
-  const bookmark = await Db.bookmark(articleId, req.user.id);
+  const bookmark = await Db.bookmark(req.article.id, req.user.id);
   if (!bookmark) throw new ApiError("article is not bookmarked", 400);
 
   //result
-  const result = await Db.remove(articleId);
+  const result = await Db.remove(req.article.id);
   if (result === 0) throw new ApiError("Internal server error", 500);
 
   res.status(200).json({ message: "article unbookmarked" });
@@ -40,9 +36,7 @@ export const bookmarks = asyncHandler(async (req, res) => {
 
 //**Get a bookmarked article by Id */
 export const bookmark = asyncHandler(async (req, res) => {
-  const { articleId } = req.params;
-
-  const bookmark = await Db.bookmark(articleId, req.user.id);
+  const bookmark = await Db.bookmark(req.article.id, req.user.id);
   if (!bookmark) throw new ApiError("article is not bookmarked", 400);
 
   res.status(200).json({ bookmark });
