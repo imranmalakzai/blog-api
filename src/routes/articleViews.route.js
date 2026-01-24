9766bf0fe9cade48f9b3edbb521c05e8b0c83f65
imranmalakzai <afghanimran92@gmail.com>
2026-01-24 21:20:38 +0430

    fix : correct undefine params issue with mergeParasm true

--- src/routes/articleViews.route.js
+++ src/routes/articleViews.route.js
@@ -1,7 +1,7 @@
 import e from "express";
 import * as Views from "../controllers/article_views.controller.js";
 
-const articleViewRouter = e.Router();
+const articleViewRouter = e.Router({ mergeParams: true });
 
 // articles/:articleSlug/views
 articleViewRouter.route("/").get(Views.articleViews);
